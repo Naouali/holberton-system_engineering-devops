88c7f68740ca495dfdf55078705ef0160360a842
naouali <elnaoualinebil@gmail.com>
2020-03-23 18:03:40 +0100

    ensure

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { '/tmp/Holberton':
-	path => '/tmp/holberton',
-	ensure => 'present',
-	content => 'I Love Puppet',
-	group => 'www-data',
-	owner => 'www-data',
-	mode => '0744',
+# create a file
+file {'/tmp/holberton':
+path    => '/tmp/holberton',
+ensure  => 'present',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
