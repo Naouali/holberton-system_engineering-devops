203435a664ece5965c6f0416d3ae12c0e0700c35
Naouali <elnaoualinebil@gmail.com>
2020-05-16 10:09:18 +0100

    if name = main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,21 +4,21 @@ fetch data from
 an api
 """
 
-
-import requests
-import sys
-id = sys.argv[1]
-data = requests.get(
+if __name__ == "__main__":
+    import requests
+    import sys
+    id = sys.argv[1]
+    data = requests.get(
         'https://jsonplaceholder.typicode.com/users/' + id)
-name = data.json()["name"]
-todo = requests.get(
+    name = data.json()["name"]
+    todo = requests.get(
         'https://jsonplaceholder.typicode.com/users/'+name+'/todos')
-total = len(todo.json())
-number = 0
-for i in todo.json():
-    if i['competed'] is True:
-        number += 1
-print('Employee {} is done with tasks({}/{}):'.format(name, number, total))
-for task in todo.json():
+    total = len(todo.json())
+    number = 0
+    for i in todo.json():
+        if i['competed'] is True:
+            number += 1
+    print('Employee {} is done with tasks({}/{}):'.format(name, number, total))
+    for task in todo.json():
         if task['completed'] is True:
             print("\t {}".format(task['title']))
