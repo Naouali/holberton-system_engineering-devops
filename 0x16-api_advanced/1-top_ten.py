1059b43078b6ff286596fea5aa7719e041c0c9ba
Naouali <elnaoualinebil@gmail.com>
2020-05-29 14:31:44 +0100

    top 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+module to return
+top 10 hot reddit posts
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    return to 10 posts
+    """
+    headers = {'user-agent': 'linux'}
+    url = 'https://api.reddit.com/r/' + subreddit
+    res = requests.get(url, headers=headers)
+    try:
+        data = res.json()
+    except:
+        print("None")
+        return
+    for i in range(0, 10):
+        try:
+            print(data['data']['children'][i]['data']['title'])
+        except:
+            print('None')
